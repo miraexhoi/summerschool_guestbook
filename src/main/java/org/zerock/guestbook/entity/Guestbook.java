80d4b7655759e2ccbaa0f2b520ca4521e027298d
최미래 <miraexhoi@gmail.com>
2023-08-10 16:17:38 +0900

    Initial commit

---
+++ src/main/java/org/zerock/guestbook/entity/Guestbook.java
@@ -0,0 +1,29 @@
+package org.zerock.guestbook.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tbl_guestbook")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+// Setter,Getter x in Entity
+public class Guestbook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long gno;
+    @Column(length = 100, nullable = false)
+    private String title;
+    @Column(length = 200, nullable = false)
+    private String content;
+    @Column(length = 50, nullable = false)
+    private String writer;
+
+    @LastModifiedDate
+    private LocalDateTime modDate;
+}
