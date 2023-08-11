10473a7aec2751512910c698a60dc5b72774b938
최미래 <miraexhoi@gmail.com>
2023-08-11 14:08:41 +0900

    8/11 class

--- src/main/java/org/zerock/guestbook/entity/Guestbook.java
+++ src/main/java/org/zerock/guestbook/entity/Guestbook.java
@@ -12,8 +12,9 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
-// Setter,Getter x in Entity
-public class Guestbook {
+@Getter
+// Setter x in Entity
+public class Guestbook extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long gno;
@@ -24,14 +25,13 @@ public class Guestbook {
     @Column(length = 50, nullable = false)
     private String writer;
 
-    public void changeTitle(String title){
+    public void changeTitle(String title) {
         this.title = title;
     }
 
-    public void changeContent(String title){
+    public void changeContent(String content) {
         this.content = content;
     }
 
-    @LastModifiedDate
-    private LocalDateTime modDate;
+
 }
